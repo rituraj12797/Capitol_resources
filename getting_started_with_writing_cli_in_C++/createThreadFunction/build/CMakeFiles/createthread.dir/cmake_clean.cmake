18403b57bc157867454ddb09ae86cf910cfdba63
Ritu Raj <alchemyrecloak@gmail.com>
2025-12-26 12:04:54 +0530

    thread affinity done

---
+++ getting_started_with_writing_cli_in_C++/createThreadFunction/build/CMakeFiles/createthread.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/createthread.dir/source/main.cpp.obj"
+  "CMakeFiles/createthread.dir/source/main.cpp.obj.d"
+  "createthread.exe"
+  "createthread.exe.manifest"
+  "createthread.pdb"
+  "libcreatethread.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/createthread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
