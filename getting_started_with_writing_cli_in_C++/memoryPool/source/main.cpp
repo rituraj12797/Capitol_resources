e5e2030335316febcbe18579da7545cb241c5a29
Ritu Raj <alchemyrecloak@gmail.com>
2025-12-28 13:05:39 +0530

    constructor for mempool

--- getting_started_with_writing_cli_in_C++/memoryPool/source/main.cpp
+++ getting_started_with_writing_cli_in_C++/memoryPool/source/main.cpp
@@ -7,8 +7,8 @@ template<typename T> class MemPool {
     // memblock struct
     private : 
     struct ObjectBlock{
-        bool is_occupied = false;
         T object;
+        bool is_occupied = false;
     }
 
     // the actual array of struct 
@@ -19,7 +19,26 @@ template<typename T> class MemPool {
     explicit MemPool(size_t element_count){
         // constructor 
         // explicit used to disable implicit conversion of arguements, preventing undefined behavior when argument types dont match and simply resulting in an error
+        store(element_count,{T(),true}); // initialize store with the expected number of object 
         
+        // Assert here t check that the first member of ObjectBool block is the T object and not the flag 
+        // benefits mentioned in diary on page 1
+        ASSERT(reinterpret_cast<*ObjectBlock> &(store[0].object) == &(store[0]), " ERROR : First member of ObjectBlock must be the template object \n");
+        // what does reinterpret_cast do ==> changes the type of pointer from one to other 
+        // syntactical use involves
+
+        // reinterpret_cast<*target_pointer_type> &(original pointer)
+        // ==> this keeps the address pointer by hexadecimal &(original pointer) as same, but just changes it's type from whetever it was to tyeh target_pointer_type 
+        // we used it here to change the type of POINTER_TO_USER_OBJECT to POINTER_TO_OBJECT_BLOCK 
+
+        // what this asert does ?? => this makes sure that the address of first stored user Object and the first ObjectBlock is same;
+        
+        // delete unnecessary other constructors to prevent any undefined conversion and behaviors, also this enforces that only a single manager(constructor) manages the initialization
+        MemPool() = delete;
+        MemPool(const MemPool&) = delete;
+        MemPool(const MemPool&&) = delete;
+        MemPool& operator=(const MemPool&) = delete;
+        MemPool& operator=(const MemPool&&) = delete;
     }
 };
 
