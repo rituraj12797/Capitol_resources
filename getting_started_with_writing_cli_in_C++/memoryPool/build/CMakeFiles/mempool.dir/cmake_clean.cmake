fa75094554926725733e5b4e8792bb1047484536
rituraj12797 <alchemyrecloak@gmail.com>
2026-01-02 19:36:33 +0530

    minor improvements and syntactical fixes in mempool file

---
+++ getting_started_with_writing_cli_in_C++/memoryPool/build/CMakeFiles/mempool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mempool.dir/source/main.cpp.o"
+  "CMakeFiles/mempool.dir/source/main.cpp.o.d"
+  "mempool"
+  "mempool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mempool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
